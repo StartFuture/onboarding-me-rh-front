f5534a773b2b59beee80ce755cde2e7af309485b
Gabriel Vinicius de Deus Sousa <111321165+GabrielHalls@users.noreply.github.com>
2023-08-04 23:11:38 -0300

    created the structure of the registration data form

--- src/pages/FormsCompany/index.jsx
+++ src/pages/FormsCompany/index.jsx
@@ -1,8 +1,42 @@
+import { Title } from "./assets/css/style.ts";
 const FormsCompany = () => {
   return (
-    <>
-    <h1>Oi</h1>
-    </>
+  <>
+    <div className="Title">
+
+      <h1>Cadastre sua empresa :) </h1> 
+      <span>Campos Obrigatórios sinalizado com *</span>
+    </div>
+
+    <div className="form">
+
+      <form action="">
+        <h4>Nome Fantasia</h4>
+        <input type="text"placeholder="Digite aqui" />
+        <h4>Razão Social</h4>
+        <input type="text" placeholder="Digite aqui" />
+        <h4>CNPJ</h4>
+        <input type="text" placeholder="Digite aqui" />
+        <h4>Inscrição Estadual</h4>
+        <input type="text" placeholder="Digite aqui" />
+      </form>
+    </div>
+
+    <div className="send-soon">
+      <span>Envie o logo da sua empresa para uma maior identificação</span>
+    </div>
+
+    <div className="checkbox">
+      <input type="checkbox"/>
+      <input type="checkbox"/>
+      <input type="checkbox"/>
+    </div>
+
+    <div className="button">
+      <button>Prosseguir</button>
+    </div>
+
+  </>
   );
 };
 

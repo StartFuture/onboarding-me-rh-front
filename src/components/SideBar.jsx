fad0139f458c96073272315e9178669e391c612e
Bruno Bergamasco <brunobergamasco05@gmail.com>
2023-08-11 17:59:35 -0300

    Alteraçao de imagens para constantes e css

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,8 +1,21 @@
 const SideBar = () => {
     return (
     <>
+
   <nav>
     <div class="sidebar">
+      <div class="status-itens"> 
+      <svg xmlns="http://www.w3.org/2000/svg" width="36" height="35" viewBox="0 0 36 35" fill="none">
+    <circle cx="18" cy="17.5" r="17" fill="white" stroke="white"/>
+  </svg>
+  <svg xmlns="http://www.w3.org/2000/svg" width="36" height="35" viewBox="0 0 36 35" fill="none">
+    <circle cx="18" cy="17.5" r="17" fill="white" stroke="white"/>
+  </svg>
+      <svg xmlns="http://www.w3.org/2000/svg" width="36" height="35" viewBox="0 0 36 35" fill="none">
+    <circle cx="18" cy="17.5" r="17" stroke="white"/>
+    <circle cx="18" cy="17.5" r="9.5" fill="white"/>
+  </svg>
+</div>
       <ul class="nav-itens">
         <li><a href="">Dados Cadastrais</a></li>
         <li><a href="">Dados De Acesso</a></li>

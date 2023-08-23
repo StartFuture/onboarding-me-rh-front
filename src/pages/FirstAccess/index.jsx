42abaec19c816419d51f82a876b4a3dba133cc63
Bruno Bergamasco <brunobergamasco05@gmail.com>
2023-08-23 17:44:02 -0300

    Tela de primeiro acesso do usuario finalizada

---
+++ src/pages/FirstAccess/index.jsx
@@ -0,0 +1,26 @@
+import "./assets/css/style.css";
+import SideBar from "../../components/SideBar";
+
+const FirstAccess = () => {
+    return (
+      <>
+      {/* <SideBar/> */}
+      <div class="container-form">
+      <div class="bar-mobile"></div>
+        <div class="content-form">
+          <h3>Informe o seu acesso</h3>
+          <h5>Campos obrigatórios sinalizados com <strong>*</strong></h5>
+          <form>
+            <h4>Senha<strong> *</strong></h4>
+            <input type="text" placeholder="Digite aqui" />
+            <h4>Confirmar senha<strong> *</strong></h4>
+            <input type="text" placeholder="Digite aqui" />
+          </form>
+          <button class= "next-button">Prosseguir</button>
+        </div>
+      </div>
+</>
+  );
+};
+
+export default FirstAccess;

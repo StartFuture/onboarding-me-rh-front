b8f0debe87b22b8ad3fa396bba2ad30026729661
GabrielHalls <gabriel.deussousa@hotmail.com>
2023-08-30 15:27:44 -0300

    update style

--- src/pages/FormCadFunci/index.jsx
+++ src/pages/FormCadFunci/index.jsx
@@ -6,119 +6,123 @@ const FormsCadFunci = () => {
   return (
 
     <>
+
       <div className="conteudo">
         <Timeline />
 
 
 
-        <form className="cadastroFuncionario">
+        <div className="formcadfuncionario">
 
-          <div className="titleCad">
-            <h4>
-              Dados Pessoais
-            </h4>
-          </div>
+          <form className="formcontent">
 
-          <div className="namecomplete">
-            <h4>Nome completo</h4>
-            <input type="text" placeholder="Digite aqui" />
-          </div>
-
-          <div className="cpfdate">
-            <div>
-              <h4>CPF</h4>
-              <input type="text" placeholder="Digite aqui" />
-            </div>
-            <div>
-              <h4>Data de nascimento</h4>
-              <input type="text" placeholder="Digite aqui" />
-            </div>
-          </div>
-
-          <div className="cargo">
-            <h4>Cargo</h4>
-            <input type="text" placeholder="Digite aqui" />
-          </div>
-
-          <div className="emailphone" >
-            <div>
-              <h4>Email</h4>
-              <input type="text" placeholder="Digite aqui" />
-            </div>
-
-            <div>
-              <h4>Telefone</h4>
-              <input type="text" placeholder="Digite aqui" />
-            </div>
-          </div>
-
-          <div className="codeState">
-            <div>
-              <h4>CEP</h4>
-              <input type="text" placeholder="Digite aqui" />
-            </div>
-
-            <div>
-              <h4>Estado</h4>
-              <input type="text" placeholder="Digite aqui" />
-            </div>
-          </div>
-
-          <div className="citystreet">
-            <div>
-              <h4>Cidade</h4>
-              <input type="text" placeholder="Digite aqui" />
-            </div>
-
-            <div>
-              <h4>Rua</h4>
-              <input type="text" placeholder="Digite aqui" />
-            </div>
-          </div>
-
-          <div className="numbercomplement">
-            <div>
-              <h4>Número</h4>
-              <input type="text" placeholder="Digite aqui" />
-            </div>
-
-            <div>
-              <h4>Complemento</h4>
-              <input type="text" placeholder="Digite aqui" />
-            </div>
-          </div>
-
-          <div className="welcometitle">
-          <h4>
-            Welcome Kits
-          </h4>
-          </div>
-
-          <div className="kits">
-            <div>
+            <div className="titleCad">
               <h4>
-                kits
+                Dados Pessoais
               </h4>
-              <input type="text" placeholder="Selecione aqui" />
             </div>
 
-            <div>
-              <h4>Status</h4>
-              <input type="text" placeholder="Selecione aqui" />
+            <div className="namecomplete">
+              <h4>Nome completo</h4>
+              <input type="text" placeholder="Digite aqui" />
             </div>
-          </div>
+
+            <div className="flexinput">
+              <div>
+                <h4>CPF</h4>
+                <input type="text" placeholder="Digite aqui" />
+              </div>
+              <div>
+                <h4>Data de nascimento</h4>
+                <input type="text" placeholder="Digite aqui" />
+              </div>
+            </div>
+
+            <div className="cargo">
+              <h4>Cargo</h4>
+              <input type="text" placeholder="Digite aqui" />
+            </div>
+
+            <div className="flexinput" >
+              <div>
+                <h4>Email</h4>
+                <input type="text" placeholder="Digite aqui" />
+              </div>
+
+              <div>
+                <h4>Telefone</h4>
+                <input type="text" placeholder="Digite aqui" />
+              </div>
+            </div>
+
+            <div className="flexinput">
+              <div>
+                <h4>CEP</h4>
+                <input type="text" placeholder="Digite aqui" />
+              </div>
+
+              <div>
+                <h4>Estado</h4>
+                <input type="text" placeholder="Digite aqui" />
+              </div>
+            </div>
+
+            <div className="flexinput">
+              <div>
+                <h4>Cidade</h4>
+                <input type="text" placeholder="Digite aqui" />
+              </div>
+
+              <div>
+                <h4>Rua</h4>
+                <input type="text" placeholder="Digite aqui" />
+              </div>
+            </div>
+
+            <div className="flexinput">
+              <div>
+                <h4>Número</h4>
+                <input type="text" placeholder="Digite aqui" />
+              </div>
+
+              <div>
+                <h4>Complemento</h4>
+                <input type="text" placeholder="Digite aqui" />
+              </div>
+            </div>
+
+            <div className="welcometitle">
+              <h4>
+                Welcome Kits
+              </h4>
+            </div>
+
+            <div className="flexinput">
+              <div>
+                <h4>
+                  kits
+                </h4>
+                <input type="text" placeholder="Selecione aqui" />
+              </div>
+
+              <div>
+                <h4>Status</h4>
+                <input type="text" placeholder="Selecione aqui" />
+              </div>
+            </div>
 
 
-          <div className="btn">
-            <div>
-              <button>Cancelar</button>
+            <div className="btn">
+              <div>
+                <button>Cancelar</button>
+              </div>
+              <div>
+                <button id="btn-salvar">Salvar</button>
+              </div>
             </div>
-            <div>
-              <button id="btn-salvar">Salvar</button>
-            </div>
-          </div>
 
-        </form>
+          </form>
+        </div>
       </div>
     </>
   );

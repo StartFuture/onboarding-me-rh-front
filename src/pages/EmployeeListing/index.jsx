d22a68162dfe3a688e57b058e7b59b11a527c698
Bruno Bergamasco <brunobergamasco05@gmail.com>
2023-09-15 23:29:51 -0300

    inicio da tela de listagem de funcionarios e termino de tela de login

--- src/pages/EmployeeListing/index.jsx
+++ src/pages/EmployeeListing/index.jsx
@@ -6,6 +6,22 @@ const EmployeeListing = () => {
       <>
         <LateralMenu/>
         {/* <TopingMenu/> */}
+                <table>
+                  <thead>
+                    <th>Nome</th>
+                    <th>CPF</th>
+                    <th>E-mail</th>
+                    <th>Cargo</th>
+                    <th>Welcomekit</th>
+                  </thead>
+                  <tbody>
+                    <td data-title="Nome"></td>
+                    <td data-title="CPF"></td>
+                    <td data-title="E-mail"></td>
+                    <td data-title="Cargo"></td>
+                    <td data-title="Welcomekit"></td>
+                  </tbody>
+                </table>
 </>
   );
 };
